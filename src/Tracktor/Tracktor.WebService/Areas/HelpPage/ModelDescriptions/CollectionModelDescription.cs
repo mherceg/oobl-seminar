03cdb246cb4d25a2514c1b7133608f802f8291a1
Jan Kelemen <jkeleme@gmail.com>
2016-12-20 15:53:09 +0100

    add web api project

---
+++ src/Tracktor/Tracktor.WebService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Tracktor.WebService.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
