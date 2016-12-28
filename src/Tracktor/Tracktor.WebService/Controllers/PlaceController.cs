d8c5c4f04f819af0081ad95b91f72ddf8a2a8234
Roko <roko.zubcic@fer.hr>
2016-12-28 20:43:47 +0100

    Added Web Service controller for places

---
+++ src/Tracktor/Tracktor.WebService/Controllers/PlaceController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tracktor.Business;
+using Tracktor.Business.Interface;
+using Tracktor.Domain;
+
+namespace Tracktor.WebService.Controllers
+{
+    public class UserController : ApiController
+    {
+        public int Add(UserEntity user)
+        {
+            try
+            {
+                IUserServices us = ServiceFactory.getUserServices();
+                return us.Register(user);
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+
+        public UserEntity Get(int id)
+        {
+            return new UserEntity()
+            {
+                FullName = "Mirko Fodor",
+                Id = 12,
+                IsActive = true,
+                Username = "mirko.fodor@gmail.com",
+                UserTypeId = 0
+            };
+        }
+
+        public UserEntity Update(UserEntity user)
+        {
+            return new UserEntity()
+            {
+                FullName = "Mirko Fodor",
+                Id = 12,
+                IsActive = true,
+                Username = "mirko.fodor@gmail.com",
+                UserTypeId = 0
+            };
+        }
+
+        public IEnumerable<UserEntity> List()
+        {
+            List<UserEntity> ret = new List<UserEntity>();
+            ret.Add(new UserEntity()
+            {
+                FullName = "Mirko Fodor",
+                Id = 12,
+                IsActive = false,
+                Username = "mirko.fodor@gmail.com",
+                UserTypeId = 0
+            });
+            ret.Add(new UserEntity()
+            {
+                FullName = "Slavko Fodor",
+                Id = 14,
+                IsActive = false,
+                Username = "slavko.fodor@gmail.com",
+                UserTypeId = 0
+            });
+
+            return ret;
+        }
+    }
+}
