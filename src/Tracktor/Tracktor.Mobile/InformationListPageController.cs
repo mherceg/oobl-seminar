963916a154ab630685331859c41e2a16f8dda1bb
Marko Stanic <mstani19@gmail.com>
2017-01-27 21:16:56 +0100

    Information screen

---
+++ src/Tracktor/Tracktor.Mobile/InformationListPageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Tracktor.Domain;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace Tracktor.Mobile
+{
+    class InformationListPageController
+    {
+        private InformationListPage page = null;  
+
+        public InformationListPageController(InformationListPage page)
+        {
+            this.page = page;            
+        }
+        public async void InitInformations(PlaceEntity)
+        {
+        }
+    }
+}
