d7e7f544657bcca145ad585e0bb2895d4394053a
Jan Kelemen <jkeleme@gmail.com>
2017-01-28 17:51:50 +0100

    implement login/logout

--- src/Tracktor/Tracktor.Web/Controllers/UserController.cs
+++ src/Tracktor/Tracktor.Web/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tracktor.Business;
+using Tracktor.Domain;
 using Tracktor.Web.ViewModels.User;
 
 namespace Tracktor.Web.Controllers
@@ -19,12 +21,26 @@ namespace Tracktor.Web.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult LogIn(LogInVM vm)
         {
-            return RedirectToAction("Search", "Event");
+            var userService = ServiceFactory.getUserServices();
+            var le = new LoginEntity { Username = vm.Username, Password = vm.Password };
+            try
+            {
+                var userId = userService.Login(le);
+                var user = userService.Get(userId);
+                Session["user"] = user;
+                return RedirectToAction("Search", "Event");
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+                return View(vm);
+            }
         }
 
         //GET: LogOut
         public ActionResult LogOut()
         {
+            Session["user"] = null;
             return RedirectToAction(nameof(LogIn));
         }
     }
